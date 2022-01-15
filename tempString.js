f3941fe12dd395df405667bd6cb3af6f69f20f88
JyotiParit <jyotiparit88@gmail.com>
2022-01-15 19:12:23 +0530

    Template String1

---
+++ tempString.js
@@ -0,0 +1,6 @@
+var employee = {
+    name: "Jyoti",
+    age: "33"
+};
+var templateString = "Name:".concat(employee.name, "\nAge:").concat(employee.age, "\n");
+console.log(templateString);
---
+++ tempString.ts
@@ -0,0 +1,8 @@
+let employee={
+    name:"Jyoti",
+    age:"33"
+}
+let templateString=`Name:${employee.name}
+Age:${employee.age}
+`;
+console.log(templateString);
